129903712f251e15751ea408bef56c55e8b3e29b
Johan Haals <johan.haals@gmail.com>
2018-01-22 22:01:11 +0100

    rename compiled binary

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [ "$S3_BUCKET" == "" ]; then
+echo "specify S3_BUCKET environment variable"
+exit 1
+fi
+
 (cd create;GOOS=linux go build -o ../createHandler;)
-(cd get;GOOS=linux go build -o ../main;)
+(cd get;GOOS=linux go build -o ../getHandler;)
 
 zip deployment.zip main createHandler
 aws cloudformation package --template-file template.yml --s3-bucket "$S3_BUCKET" --output-template-file packaged.yml
